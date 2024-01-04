75579f0eb02b490ed56f93c6372893db65146e85
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-01-04 15:15:19 -0600

    Add projects components and update angular.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { NoteComponent } from './components/note/note.component';
+import { NoteAddComponent } from './components/note/note-add/note-add.component';
+import { NoteEditComponent } from './components/note/note-edit/note-edit.component';
+import { NoteListComponent } from './components/note-list/note-list.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    NoteComponent,
+    NoteAddComponent,
+    NoteEditComponent,
+    NoteListComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/note/note-add/note-add.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-note-add',
+  templateUrl: './note-add.component.html',
+  styleUrl: './note-add.component.css'
+})
+export class NoteAddComponent {
+
+}
