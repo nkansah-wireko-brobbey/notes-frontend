aaa766d7996035029c284169aaa504616e7422ca
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-01-04 17:35:07 -0600

    Add FormsModule to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // 
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +12,8 @@ import { NoteAddComponent } from './components/note/note-add/note-add.component'
 import { NoteEditComponent } from './components/note/note-edit/note-edit.component';
 import { NoteListComponent } from './components/note-list/note-list.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+
+
 
 @NgModule({
   declarations: [
@@ -24,6 +28,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
