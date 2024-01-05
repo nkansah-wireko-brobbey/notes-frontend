6a0a4317e3399e8c584223691556962fd48b734a
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-01-04 18:56:57 -0600

    Add signup route to app-routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 const routes: Routes = [
   
   { path: 'login', 
   component: LoginComponent,
-  pathMatch: 'full' }
+  pathMatch: 'full' },
+  
+  { path: 'signup',
+  component: SignupComponent,
+  pathMatch: 'full' },
+  
 ];
 
 @NgModule({
