7c7eee59c5d809ca5dd3d4da8f38c8194ce88693
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-01-05 20:37:41 -0600

    Add navbar and note list components to app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NoteListComponent } from './components/note-list/note-list.component';
 
 const routes: Routes = [
   
@@ -12,7 +14,22 @@ const routes: Routes = [
   { path: 'signup',
   component: SignupComponent,
   pathMatch: 'full' },
+
+  { path: '',
+redirectTo: '/login',
+pathMatch: 'full',
+},
+{
+  path: '',
+  component: NavbarComponent,
+  children: [ 
+    {
+      path: 'notes',
+      component: NoteListComponent
+    },
   
+  ]
+}
 ];
 
 @NgModule({
