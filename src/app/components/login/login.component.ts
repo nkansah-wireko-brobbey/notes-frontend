b2571caf021a49c86f69332468d1a952a7135141
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-01-05 20:42:37 -0600

    Add success modal to login button

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { FormBuilder, FormGroup, Validator } from '@angular/forms';
+import { ModalService } from '../../services/modal.service';
+import { modalData } from '../../models/ui';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +10,38 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  modalTitle: string = 'Success!';
+  modalBody: string = 'You have successfully logged in!';
+
+  modalData : modalData = {
+    title: 'Success!',
+    body: 'You have successfully logged in!',
+    buttonText: 'Close',
+    modalType: 'success'
+  }
+
+  constructor(private authService: AuthService, private modalService: ModalService) { }
+
+  ngOnInit(){
+    this.test();
+  }
+
+  public test(){
+    this.modalService.showSuccessModal(this.modalData);
+    
+    this.authService.test().subscribe(
+      res => {
+        console.log(res);
+      }
+    );
+  }
+
+  public showSuccessModal(){
+
+    console.log('showing modal');
+
+    // this.modalService.showSuccessModal();
+
+  }
 
 }
