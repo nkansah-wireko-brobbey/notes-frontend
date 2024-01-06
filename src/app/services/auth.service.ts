799834d4f1214987020d445c18cfcdac0c5394d5
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-01-05 20:40:14 -0600

    Add HttpClient and API methods to AuthService

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../env/environment';
+import { Observable } from 'rxjs';
+import { SignupRequest } from '../models/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+    // Define API
+    apiURL = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  public test(): Observable<any>{
+    return this.http.get(`${this.apiURL}/test/hello`);
+  }
+
+  public signup(data: SignupRequest): Observable<any>{
+    return this.http.post(`${this.apiURL}/users/signup`, data);
+  }
+    
 }
