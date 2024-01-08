e16a1247f76706b2189a8d6fde84c256e8be0b1d
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-01-08 11:22:39 -0600

    Update import statement for HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'; // 
-import { HttpClientModule, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HTTP_INTERCEPTORS,HttpClientModule, provideHttpClient, withInterceptors } from '@angular/common/http';
 
 
 import { AppRoutingModule } from './app-routing.module';
