df011dda9b0db2668bd77338844a6e77721bac69
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-01-09 16:22:30 -0600

    Add authentication check in auth guard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,5 +1,15 @@
 import { CanActivateFn } from '@angular/router';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth/auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
+  const authService = new AuthService();
+  const router = new Router();
+
+  if(!authService.isUserLoggedIn()){
+    console.log('user not logged in');
+     router.navigate(['login']);
+  }
+  console.log('user logged in');
   return true;
 };
