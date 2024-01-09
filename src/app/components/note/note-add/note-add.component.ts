1ec3aed6e0edc5621da5c498034661bdd667f05c
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-01-09 16:21:15 -0600

    Add note-add component with form and submit functionality

--- src/app/components/note/note-add/note-add.component.ts
+++ src/app/components/note/note-add/note-add.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { NoteService } from '../../../services/note.service';
+import { firstValueFrom } from 'rxjs';
+import { modalData } from '../../../models/ui';
+import { ModalService } from '../../../services/modal.service';
 
 @Component({
   selector: 'app-note-add',
@@ -7,4 +12,43 @@ import { Component } from '@angular/core';
 })
 export class NoteAddComponent {
 
+  modalData: modalData = {} as modalData; 
+
+
+  noteForm: FormGroup = this.formBuilder.group({
+    title: ['', Validators.required],
+    content: ['', Validators.required]
+  });
+
+constructor(
+  private formBuilder: FormBuilder,
+  private noteService: NoteService,
+  private modalService: ModalService
+ 
+  ){
+  }
+  
+  
+     async onsubmit(){
+        
+      try{
+
+        const note = await firstValueFrom(this.noteService.saveNote(
+          this.noteForm.value
+        ));
+        console.log(note);
+      }catch(error: any){
+        this.modalData = {
+          title: 'Error!',
+          body: error.message,
+          buttonText: 'Close',
+          modalType: 'error'
+        }
+        this.modalService.showSuccessModal(this.modalData);
+        console.log(error);
+
+      }
+        
+        console.log(this.noteForm.value);
+      }
 }
