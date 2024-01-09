a97988926ca51e6c5a7dbea102da0ac43c5a2228
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-01-09 16:22:16 -0600

    Add NoteService and implement getNotes method

--- src/app/components/note-list/note-list.component.ts
+++ src/app/components/note-list/note-list.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NoteService } from '../../services/note.service';
+import { firstValueFrom } from 'rxjs';
+import { Note } from '../../models/note';
 
 @Component({
   selector: 'app-note-list',
   templateUrl: './note-list.component.html',
   styleUrl: './note-list.component.css'
 })
-export class NoteListComponent {
+export class NoteListComponent implements OnInit {
 
+  notes: Note[] = []; 
+
+    ngOnInit(): void {
+        this.getNotes();
+    }
+
+    constructor(
+      private noteService: NoteService
+    ) { }
+
+   async getNotes(): Promise<any> {
+
+    try {
+
+      const response: Note[] = await firstValueFrom(this.noteService.getNotes())
+      
+      if(response){
+        this.notes = response;
+      }
+      console.log(response);
+
+    } catch (error) {
+      
+      console.log(error);
+
+    }
+
+    }
+    
 }
