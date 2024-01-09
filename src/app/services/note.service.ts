5246297391620a5dc5f46d5062cf08208addfe1b
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-01-09 16:23:13 -0600

    Add CRUD operations to NoteService

--- src/app/services/note.service.ts
+++ src/app/services/note.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../env/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteService {
 
-  constructor() { }
+  private baseApi = environment.apiUrl;
+
+  constructor(
+    private http: HttpClient,
+    
+  ) { }
+
+    public saveNote(data : any): Observable<any>{
+      return this.http.post(`${this.baseApi}/notes`, data);
+    }
+
+    public getNotes(): Observable<any>{
+      return this.http.get(`${this.baseApi}/notes`);
+    }
+
+    public getNoteById(id: string): Observable<any>{
+      return this.http.get(`${this.baseApi}/notes/${id}`);
+    }
+
+    public updateNoteById(id: string, data: any): Observable<any>{
+      return this.http.put(`${this.baseApi}/notes/${id}`, data);
+    }
+
 }
